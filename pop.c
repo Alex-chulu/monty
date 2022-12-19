1a72b60c08da8bb6b88af21018d744f28650599a
Alex-chulu <chulualexandar@gmail.com>
2022-12-19 15:59:21 +0200

    task 2

--- pop.c
+++ pop.c
@@ -5,19 +5,20 @@
  * @counter: line counter
  * Return: no
 */
+
 void f_pop(stack_t **head, unsigned int counter)
 {
-	stack_t *he;
+	stack_t *h;
 
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%d: cannot pop an empty stack\n", counter);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	he = *head;
-	*head = he->next;
-	free(he);
+	h = *head;
+	*head = h->next;
+	free(h);
 }
